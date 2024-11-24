66b6618734a9e00c84c7c4fbfd1fbdc1c44cb7b3
Gabriel Lara Baptista <gabriel.l.baptista@gmail.com>
2024-11-24 17:43:53 -0300

    CarHolding sample with Open API

---
+++ ch04/Configurations/OpenApiConfigurationOptions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.OpenApi.Models;
+
+
+namespace ch04.Configurations
+{
+    public class OpenApiConfigurationOptions : DefaultOpenApiConfigurationOptions
+    {
+        public override OpenApiInfo Info { get; set; } = new OpenApiInfo()
+        {
+            Title = "Car Holding API",
+            Description = "Car Holding APIs",
+            Version = "v1",
+            Contact = new OpenApiContact
+            {
+                Name = "Microservices Carholding",
+                Url = new System.Uri("https://www.carholding.com.br")
+            },
+        };
+
+        public override OpenApiVersionType OpenApiVersion { get; set; } = OpenApiVersionType.V2;
+    }
+}
--- ch04/Program.cs
+++ ch04/Program.cs
@@ -1,14 +1,20 @@
+using ch04;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
+    .ConfigureFunctionsWorkerDefaults(worker => worker.UseNewtonsoftJson())
+    .ConfigureOpenApi()
     .ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
     })
+
     .Build();
 
 host.Run();
+
