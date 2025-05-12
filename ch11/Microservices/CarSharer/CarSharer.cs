ac2571a4985bce8d58b87757eda827cee63effd4
Gabriel Lara Baptista <gabriel.l.baptista@gmail.com>
2025-05-11 23:24:14 -0300

    Code update for car sharer

--- ch11/Microservices/CarSharer/CarSharer.cs
+++ ch11/Microservices/CarSharer/CarSharer.cs
@@ -17,7 +17,7 @@ namespace CarSharer
         }
 
         [Function("Create")]
-        [OpenApiOperation("Create", "Creates a CarSharer data")]
+        [OpenApiOperation("Create", "Creates a new route with date and all towns’ milestones")]
         [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(IActionResult))]
         [OpenApiResponseWithBody(HttpStatusCode.Unauthorized, "text/plain", typeof(string))]
         [OpenApiResponseWithBody(HttpStatusCode.NotFound, "text/plain", typeof(string))]
@@ -28,38 +28,63 @@ namespace CarSharer
             return new OkObjectResult("C# HTTP trigger function processed a post request");
         }
 
-        [Function("Read")]
-        [OpenApiOperation("Read", "Reads a CarSharer data")]
-        [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(IActionResult))]
-        [OpenApiResponseWithBody(HttpStatusCode.Unauthorized, "text/plain", typeof(string))]
-        [OpenApiResponseWithBody(HttpStatusCode.NotFound, "text/plain", typeof(string))]
-        [OpenApiResponseWithBody(HttpStatusCode.InternalServerError, "text/plain", typeof(string))]
-        public IActionResult Read([HttpTrigger(AuthorizationLevel.Function, "get", Route = "v1/Read")] HttpRequest req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a get request.");
-            return new OkObjectResult("C# HTTP trigger function processed a get request.");
-        }
-
-
-        [Function("Update")]
-        [OpenApiOperation("Update", "Updates a CarSharer data")]
-        [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(IActionResult))]
-        [OpenApiResponseWithBody(HttpStatusCode.Unauthorized, "text/plain", typeof(string))]
-        [OpenApiResponseWithBody(HttpStatusCode.NotFound, "text/plain", typeof(string))]
-        [OpenApiResponseWithBody(HttpStatusCode.InternalServerError, "text/plain", typeof(string))]
-        public IActionResult Update([HttpTrigger(AuthorizationLevel.Function, "put", Route = "v1/Update")] HttpRequest req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed an update request.");
-            return new OkObjectResult("C# HTTP trigger function processed an update request.");
-        }
 
         [Function("Delete")]
-        [OpenApiOperation("Delete", "Deletes a CarSharer data")]
+        [OpenApiOperation("Delete", "Removes a specific route")]
         [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(IActionResult))]
         [OpenApiResponseWithBody(HttpStatusCode.Unauthorized, "text/plain", typeof(string))]
         [OpenApiResponseWithBody(HttpStatusCode.NotFound, "text/plain", typeof(string))]
         [OpenApiResponseWithBody(HttpStatusCode.InternalServerError, "text/plain", typeof(string))]
         public IActionResult Delete([HttpTrigger(AuthorizationLevel.Function, "delete", Route = "v1/Delete")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a delete request.");
+            return new OkObjectResult("C# HTTP trigger function processed a delete request.");
+        }
+
+        [Function("Close")]
+        [OpenApiOperation("Close", "Closes a route to prevent further matching")]
+        [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(IActionResult))]
+        [OpenApiResponseWithBody(HttpStatusCode.Unauthorized, "text/plain", typeof(string))]
+        [OpenApiResponseWithBody(HttpStatusCode.NotFound, "text/plain", typeof(string))]
+        [OpenApiResponseWithBody(HttpStatusCode.InternalServerError, "text/plain", typeof(string))]
+        public IActionResult Close([HttpTrigger(AuthorizationLevel.Function, "post", Route = "v1/Close")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a delete request.");
+            return new OkObjectResult("C# HTTP trigger function processed a delete request.");
+        }
+
+        [Function("Extend")]
+        [OpenApiOperation("Extend", "Accepts user requests into an existing route")]
+        [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(IActionResult))]
+        [OpenApiResponseWithBody(HttpStatusCode.Unauthorized, "text/plain", typeof(string))]
+        [OpenApiResponseWithBody(HttpStatusCode.NotFound, "text/plain", typeof(string))]
+        [OpenApiResponseWithBody(HttpStatusCode.InternalServerError, "text/plain", typeof(string))]
+        public IActionResult Extend([HttpTrigger(AuthorizationLevel.Function, "post", Route = "v1/Extend")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a delete request.");
+            return new OkObjectResult("C# HTTP trigger function processed a delete request.");
+        }
+
+        [Function("GetSuggestedExtensions")]
+        [OpenApiOperation("GetSuggestedExtensions", "Lists compatible ride requests for a route")]
+        [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(IActionResult))]
+        [OpenApiResponseWithBody(HttpStatusCode.Unauthorized, "text/plain", typeof(string))]
+        [OpenApiResponseWithBody(HttpStatusCode.NotFound, "text/plain", typeof(string))]
+        [OpenApiResponseWithBody(HttpStatusCode.InternalServerError, "text/plain", typeof(string))]
+        public IActionResult GetSuggestedExtensions([HttpTrigger(AuthorizationLevel.Function, "post", Route = "v1/GetSuggestedExtensions")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a delete request.");
+            return new OkObjectResult("C# HTTP trigger function processed a delete request.");
+        }
+
+
+        [Function("GetActiveRoutes")]
+        [OpenApiOperation("GetActiveRoutes", "Lists all active (not expired or deleted) routes for a specific user.")]
+        [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(IActionResult))]
+        [OpenApiResponseWithBody(HttpStatusCode.Unauthorized, "text/plain", typeof(string))]
+        [OpenApiResponseWithBody(HttpStatusCode.NotFound, "text/plain", typeof(string))]
+        [OpenApiResponseWithBody(HttpStatusCode.InternalServerError, "text/plain", typeof(string))]
+        public IActionResult GetActiveRoutes([HttpTrigger(AuthorizationLevel.Function, "post", Route = "v1/GetActiveRoutes")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a delete request.");
             return new OkObjectResult("C# HTTP trigger function processed a delete request.");
